ed8c85decf3c3ad12fd8d4f614ffdd84fc2a8aed
Lucas Santana <lucasg4x@gmail.com>
2023-07-08 23:20:46 -0300

    comentar reinicializaçao dos clusters

--- apps/greedy_algorithms.h
+++ apps/greedy_algorithms.h
@@ -545,19 +545,19 @@ void gulosoRandomizado(Grafo *g, Cluster **clusters, int num_clusters, float min
         }
 
         // se não achou solução na ultima iteraçao, inicializa os clusters com novos ids
-        if (melhorSolucao == nullptr)
-        {
-            for (int i = 0; i < num_clusters; i++)
-            {
-                int idRandom = rand() % g->getOrdem(); // Id de um nó
-                while (nosInseridos[idRandom] != -1)
-                {
-                    idRandom = rand() % g->getOrdem();
-                }
-                inicializacaoIndices[i] = idRandom; // Para cada indice que representa o indice de um cluster, nessa posição estará o valor de qual nó que o inicializará.
-                nosInseridos[idRandom] = i;         // Na posição idRandom que é o id de um nó eu taco qual cluster que ele vai entrar
-            }
-        }
+        // if (melhorSolucao == nullptr)
+        // {
+        //     for (int i = 0; i < num_clusters; i++)
+        //     {
+        //         int idRandom = rand() % g->getOrdem(); // Id de um nó
+        //         while (nosInseridos[idRandom] != -1)
+        //         {
+        //             idRandom = rand() % g->getOrdem();
+        //         }
+        //         inicializacaoIndices[i] = idRandom; // Para cada indice que representa o indice de um cluster, nessa posição estará o valor de qual nó que o inicializará.
+        //         nosInseridos[idRandom] = i;         // Na posição idRandom que é o id de um nó eu taco qual cluster que ele vai entrar
+        //     }
+        // }
 
         k++;
     }
@@ -802,20 +802,20 @@ void gulosoRandomizadoReativo(Grafo *g, Cluster **clusters, int num_clusters, fl
             nosInseridos[i] = -1; // Resetar
         }
 
-        // se não achou solução na ultimo bloco de iteraçao, inicializa os clusters com novos ids
-        if (k % bloco == 0 && melhorSolucao == nullptr)
-        {
-            for (int i = 0; i < num_clusters; i++)
-            {
-                int idRandom = rand() % g->getOrdem(); // Id de um nó
-                while (nosInseridos[idRandom] != -1)
-                {
-                    idRandom = rand() % g->getOrdem();
-                }
-                inicializacaoIndices[i] = idRandom; // Para cada indice que representa o indice de um cluster, nessa posição estará o valor de qual nó que o inicializará.
-                nosInseridos[idRandom] = i;         // Na posição idRandom que é o id de um nó eu taco qual cluster que ele vai entrar
-            }
-        }
+        // // se não achou solução na ultimo bloco de iteraçao, inicializa os clusters com novos ids
+        // if (k % bloco == 0 && melhorSolucao == nullptr)
+        // {
+        //     for (int i = 0; i < num_clusters; i++)
+        //     {
+        //         int idRandom = rand() % g->getOrdem(); // Id de um nó
+        //         while (nosInseridos[idRandom] != -1)
+        //         {
+        //             idRandom = rand() % g->getOrdem();
+        //         }
+        //         inicializacaoIndices[i] = idRandom; // Para cada indice que representa o indice de um cluster, nessa posição estará o valor de qual nó que o inicializará.
+        //         nosInseridos[idRandom] = i;         // Na posição idRandom que é o id de um nó eu taco qual cluster que ele vai entrar
+        //     }
+        // }
 
         k++;
     }
